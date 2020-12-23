5bea1fa4c4263570f8d4f1dd75435d52d21567d8
Shubham R <74360434+ShubhamR0804@users.noreply.github.com>
2020-12-23 18:29:04 +0530

    Add files via upload

---
+++ Month And No. Of Days In Month.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 22:48:22 2020
+
+@author: Shubham Ramteke
+"""
+
+
+n=int(input("The Month No.= "))
+if(n==1):
+    print(31,"(The Month is : January)")
+elif(n==2):
+    print(29,"(28 for non leap year)","(The Month is : February)")
+elif(n==3):
+    print(31,"(The Month is : March)")
+elif(n==4):
+    print(30,"(The Month is : April)")
+elif(n==5):
+    print(31,"(The Month is : May)")
+elif(n==6):
+    print(30,"(The Month is : June)")
+elif(n==7):
+    print(31,"(The Month is : July)")
+elif(n==8):
+    print(31,"(The Month is : August)")
+elif(n==9):
+    print(30,"(The Month is : September)")
+elif(n==10):
+    print(31,"(The Month is : October)")
+elif(n==11):
+    print(30,"(The Month is : November)")
+elif(n==12):
+    print(31,"(The Month is : December)")
+else:
+    print("There are only 12 Months")
